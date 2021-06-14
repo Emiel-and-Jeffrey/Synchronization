5cd6a881604b2d9cece29b2b87f728bcd6a88de0
braggs <emielvdbrink@gmail.com>
2021-06-14 10:23:24 +0200

    brought back old version

--- Week 4/N_emiel_solution.py
+++ Week 4/N_emiel_solution.py
@@ -6,28 +6,22 @@ mutex = MyMutex("mutex")
 cv_reader = MyConditionVariable(mutex,"cv_reader")
 cv_writer = MyConditionVariable(mutex,"cv_writer")
 
-nrof_reader_busy = MyInt(0,"reader_busy")
-nrof_writer_busy = MyInt(0,"writer_busy")
-
 nrof_reader_wait = MyInt(0,"reader_wait")
 nrof_writer_wait = MyInt(0,"writer_wait")
 
-writer_prio = MyBool(True,"writer_prio")
+nrof_reader_busy = MyInt(0,"reader_busy")
+nrof_writer_busy = MyInt(0,"writer_busy")
 
-turnstile = MySemaphore(1, "turnstile")
+writer_prio = MyBool(True,"writer_prio")
 
 def reader_thread():
 	while True:
 		
-		turnstile.wait()
-		turnstile.signal()
-		
 		mutex.wait()
 		
 		nrof_reader_wait.v += 1
-		
-		# and (writer_prio and nrof_writer_wait > 0)
-		while nrof_writer_busy.v > 0:
+
+		while nrof_writer_busy.v > 0 and (writer_prio and nrof_writer_wait > 0):
 			cv_reader.wait()
 		
 		nrof_reader_wait.v -= 1
@@ -43,7 +37,6 @@ def reader_thread():
 		
 		if writer_prio.v:
 			if nr_writer_wait.v > 0:
-				turnstile.wait()
 				cv_writer.notify()
 		
 		mutex.signal()	
@@ -86,5 +79,5 @@ def writer_thread():
 def setup():
 	# subscribe_thread(reader_thread)
 	# subscribe_thread(reader_thread)
-	# subscribe_thread(reader_thread)
-	subscribe_thread(writer_thread)
+	subscribe_thread(reader_thread)
+	# subscribe_thread(writer_thread)
