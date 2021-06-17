87ae5d88bef99dbc3e4b71f737709f227eb3dbb6
braggs <emielvdbrink@gmail.com>
2021-06-17 17:13:27 +0200

    added solution to problem P

--- Week 5/P_solution.py
+++ Week 5/P_solution.py
@@ -14,13 +14,17 @@ heathens_queue_cv = MyConditionVariable(mutex, "heathens_queue")
 prudes_buffer_cv = MyConditionVariable(mutex, "prudes_buffer")
 prudes_queue_cv = MyConditionVariable(mutex, "prudes_queue")
 
-heathens = MyInt(0, "heathens")
-prudes = MyInt(0, "prudes")
+heathens_in_buffer = MyInt(0, "heathens_in_buffer")
+prudes_in_buffer = MyInt(0, "prudes_in_buffer")
 
-state = MyString("", "state")
+heathens_in_queue = MyInt(0, "heathens_in_queue")
+prudes_in_queue = MyInt(0, "prudes_in_queue")
+
+state = MyString(NEUTRAL, "state")
 
 def person_thread(
-        counter: MyInt, ncounter: MyInt,
+        buffer_counter: MyInt,
+        queue_counter: MyInt, nqueue_counter: MyInt,
         buffer_cv: MyConditionVariable,
         queue_cv: MyConditionVariable, nqueue_cv: MyConditionVariable,
         rule_state: str, nrule_state: str,
@@ -29,35 +33,21 @@ def person_thread(
 
         mutex.wait()
 
-        #first condition variable: (acts as a transition buffer to prevent newcomers adding to the queue)
-        #-heathens get stuck here if state == "transitioning to prudes"
-        #-prudes get stuck here if state == "transitioning to heathens"
+        buffer_counter.v += 1
+
         while state.v == ntransition_state:
             buffer_cv.wait()
+        
+        buffer_counter.v -= 1
 
-        counter.v += 1
+        queue_counter.v += 1
 
-        #if statements:
-        #-heathens:
-        #   if state == "neutral":
-        #       state == "heathens rule"
-        #   if state == "prudes rule" && heathens > prudes:
-        #       state = "transitioning to heathens"
-        #
-        #-prudes:
-        #   if state == "neutral":
-        #       state == "prudes rule"
-        #   if state == "heathens rule" && prudes > heathens:
-        #       state = "transitioning to prudes"
         if state.v == NEUTRAL:
-            state.v == rule_state
+            state.v = rule_state
 
-        elif state.v == nrule_state and counter.v > ncounter.v:
-            state.v == transition_state
+        elif state.v == nrule_state and queue_counter.v > nqueue_counter.v:
+            state.v = transition_state
 
-        #second condition variable: (acts as the queue for when we cannot enter the field directly)
-        #-heathens get stuck here if state == "prudes rule" || "transitioning to heathens"
-        #-prudes get stuck here if state == "heathens rule" || "transitioning to prudes"
         while state.v == nrule_state or state.v == transition_state:
             queue_cv.wait()
 
@@ -67,34 +57,29 @@ def person_thread(
 
         mutex.wait()
 
-        counter.v -= 1
+        queue_counter.v -= 1
 
-        #if statements:
-        #-heathens:
-        #   if state == "heathens rule" && heathens == 0:
-        #       state == "neutral"
-        #   if state == "heathens rule" && prudes > heathens:
-        #       state == "transitioning to prudes"
-        #   
-        #-prudes:
-        #   if state == "prudes rule" && prudes == 0:
-        #       state == "neutral"
-        #   if state == "prudes rule" && prudes > heathens:
-        #       state == "transitioning to heathens"
-        if state.v == rule_state and ncounter.v > counter.v:
-            state.v == ntransition_state
+        if state.v == rule_state and nqueue_counter.v > queue_counter.v:
+            state.v = ntransition_state
 
-        if state.v == ntransition_state and counter.v == 0:
-            state.v == nrule_state
-            nqueue_cv.notify_all()
-            buffer_cv.notify_all()
+        if state.v == ntransition_state and queue_counter.v == 0:
+            state.v = nrule_state
+            if nqueue_counter.v > 0:
+                nqueue_cv.notify_all()
+            if buffer_counter.v > 0:
+                buffer_cv.notify_all()
         
-        if state.v == rule_state and counter.v == 0:
-            state.v == NEUTRAL
+        if state.v == rule_state and queue_counter.v == 0:
+            state.v = NEUTRAL
 
         mutex.signal()
 
 def setup():
-	subscribe_thread(lambda: person_thread(heathens, prudes, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
-	subscribe_thread(lambda: person_thread(heathens, prudes, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
-	subscribe_thread(lambda: person_thread(heathens, prudes, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
+	subscribe_thread(lambda: person_thread(heathens_in_buffer, heathens_in_queue, prudes_in_queue, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
+	subscribe_thread(lambda: person_thread(heathens_in_buffer, heathens_in_queue, prudes_in_queue, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
+	subscribe_thread(lambda: person_thread(heathens_in_buffer, heathens_in_queue, prudes_in_queue, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
+	subscribe_thread(lambda: person_thread(heathens_in_buffer, heathens_in_queue, prudes_in_queue, heathens_buffer_cv, heathens_queue_cv, prudes_queue_cv, HEATHENS_RULE, PRUDES_RULE, HEATHENS_TRANSITION, PRUDES_TRANSITION))
+
+	subscribe_thread(lambda: person_thread(prudes_in_buffer, prudes_in_queue, heathens_in_queue, prudes_buffer_cv, prudes_queue_cv, heathens_queue_cv, PRUDES_RULE, HEATHENS_RULE, PRUDES_TRANSITION, HEATHENS_TRANSITION))
+	subscribe_thread(lambda: person_thread(prudes_in_buffer, prudes_in_queue, heathens_in_queue, prudes_buffer_cv, prudes_queue_cv, heathens_queue_cv, PRUDES_RULE, HEATHENS_RULE, PRUDES_TRANSITION, HEATHENS_TRANSITION))
+	subscribe_thread(lambda: person_thread(prudes_in_buffer, prudes_in_queue, heathens_in_queue, prudes_buffer_cv, prudes_queue_cv, heathens_queue_cv, PRUDES_RULE, HEATHENS_RULE, PRUDES_TRANSITION, HEATHENS_TRANSITION))
